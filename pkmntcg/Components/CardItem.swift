d81955e19c48f301e829859405198fcb538e5993
BLANCA MARIA FERNANDEZ MARTIN <blancafdezmartin1998@gmail.com>
2026-09-18 22:30:51 +0200

    clip images on illustration mode

--- pkmntcg/Components/CardItem.swift
+++ pkmntcg/Components/CardItem.swift
@@ -7,75 +7,76 @@
 import SwiftUI
 
 struct CardItem: View {
-  
-  enum Mode {
-    case full
-    case illustration
-  }
-  
-  let image: URL?
-  let mode: Mode
-  
-  var body: some View {
-    AsyncImage(url: image) { phase in
-      switch phase {
-      case .empty:
-        ProgressView()
-      case .success(let image):
-        image
-          .resizable()
-          .aspectRatio(
-            mode == .full ? 0.716 : 1,
-            contentMode: .fit
-          )
-          .modifier(
-            CardImageModifier(mode: mode)
-          )
-      case .failure(let error):
-        VStack(spacing: 8) {
-          Image(systemName: "photo")
-            .font(.largeTitle)
-          
-          Text("Error")
-          Text(error.localizedDescription)
-            .font(.caption)
-            .multilineTextAlignment(.center)
-          
-          Text(image?.absoluteString ?? "Sin URL")
-            .font(.caption2)
-            .foregroundStyle(.secondary)
-        }
-        
-      @unknown default:
-        EmptyView()
-      }
+
+    enum Mode {
+        case full
+        case illustration
     }
-    .clipShape(RoundedRectangle(cornerRadius: 12))
-    .shadow(
-      color: .black.opacity(0.25),
-      radius: 6,
-      x: 0,
-      y: 3
-    )
-  }
+
+    let image: URL?
+    let mode: Mode
+
+    var body: some View {
+        AsyncImage(url: image) { phase in
+            switch phase {
+
+            case .empty:
+                ProgressView()
+
+            case .success(let image):
+                image
+                    .resizable()
+                    /*.aspectRatio(
+                        mode == .full ? 0.716 : 1,
+                        contentMode: .fit
+                    )*/
+                    .modifier(
+                        CardImageModifier(mode: mode)
+                    )
+
+            case .failure(let error):
+                VStack(spacing: 8) {
+                    Image(systemName: "photo")
+                        .font(.largeTitle)
+                }
+
+            @unknown default:
+                EmptyView()
+            }
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 6))
+        .shadow(
+            color: .black.opacity(0.5),
+            radius: 3,
+            x: 0,
+            y: 3
+        )
+    }
 }
 
 struct CardImageModifier: ViewModifier {
-  
-  let mode: CardItem.Mode
-  
-  func body(content: Content) -> some View {
-    switch mode {
-    case .full:
-      content
-        .scaledToFit()
-      
-    case .illustration:
-      content
-        .scaledToFill()
-        .frame(maxWidth: .infinity)
-        .aspectRatio(1, contentMode: .fit)
-        .clipped()
+
+    let mode: CardItem.Mode
+
+    func body(content: Content) -> some View {
+
+        switch mode {
+
+        case .full:
+            content
+                .scaledToFit()
+
+        case .illustration:
+            content
+                .scaledToFill()
+                .scaleEffect(1.19)
+                .offset(y: 64)
+                .frame(height: 110)
+                .clipped()
+        }
     }
-  }
+}
+
+#Preview {
+  SearchView()
 }
